9c9230ea09e2772a7e4c96178e4aa9aace9f9df5
Sven Giebelmann <svengiebelmann@gmail.com>
2024-03-24 16:25:45 +0100

    feat: lock cache file to prevent race condition

--- FileCache.cs
+++ FileCache.cs
@@ -1,35 +1,65 @@
+using System.Collections.Concurrent;
 using System.Net;
 using System.Text;
-using System.Text.Json;
 
-class FileCache: DelegatingHandler
+class FileCache : DelegatingHandler
 {
+    private readonly SemaphoreSlim _semaphoreSlim = new(1, 1);
+    private ConcurrentDictionary<string, SemaphoreSlim> _locks = new();
+
     protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         return SendAsync(request, cancellationToken).Result;
     }
 
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
     {
-        var cachePath = Path.Join(Directory.GetCurrentDirectory(), "Cache", request.RequestUri!.PathAndQuery).TrimEnd('/') + ".json";
+        var cachePath = Path.Join(Directory.GetCurrentDirectory(), "Cache", request.RequestUri!.PathAndQuery)
+            .TrimEnd('/') + ".json";
+        SemaphoreSlim fileLock = _semaphoreSlim;
+
         if (File.Exists(cachePath))
         {
-            // TODO: try with stream content
-            var content = File.ReadAllText(cachePath);
-            return new HttpResponseMessage(HttpStatusCode.OK)
+            string content;
+            await fileLock.WaitAsync(cancellationToken);
+
+            try
             {
-                Content = new StringContent(content, Encoding.UTF8, "application/json")
-            };
+                content = await File.ReadAllTextAsync(cachePath, cancellationToken);
+            }
+            finally
+            {
+                fileLock.Release();
+            }
+
+            if (!string.IsNullOrWhiteSpace(content))
+            {
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent(content, Encoding.UTF8, "application/json")
+                };
+            }
         }
+
         var response = await base.SendAsync(request, cancellationToken);
         if (!response.IsSuccessStatusCode)
         {
             return response;
         }
-    
-        Directory.CreateDirectory(Path.GetDirectoryName(cachePath)!);
-        await File.WriteAllTextAsync(cachePath, await response.Content.ReadAsStringAsync(cancellationToken), cancellationToken);
-        
+
+        await fileLock.WaitAsync(cancellationToken);
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(cachePath)!);
+            await File.WriteAllTextAsync(cachePath, await response.Content.ReadAsStringAsync(cancellationToken),
+                cancellationToken);
+        }
+        finally
+        {
+            fileLock.Release();
+        }
+
         return response;
     }
 };
