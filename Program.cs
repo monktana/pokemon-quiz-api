480fe56b544139190503f0ba5b2113cccce38522
Sven Giebelmann <svengiebelmann@gmail.com>
2024-03-24 12:01:20 +0100

    feat: add PokeApiService and FileCache to builder

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using PokeQuiz.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient<PokeApiService>().AddHttpMessageHandler(config => new FileCache());
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (PokeApiService service) => await service.GetMatchup());
 
 app.Run();
