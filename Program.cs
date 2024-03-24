f1ab6252e1bebd6f8ca290f6d4c4c3eb1b733cf6
Sven Giebelmann <svengiebelmann@gmail.com>
2024-03-24 16:26:49 +0100

    feat: rename used service

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 using PokeQuiz.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpClient<PokeApiService>().AddHttpMessageHandler(config => new FileCache());
+builder.Services.AddHttpClient<PokeQuizService>().AddHttpMessageHandler(config => new FileCache());
 
 var app = builder.Build();
 
-app.MapGet("/", async (PokeApiService service) => await service.GetMatchup());
+app.MapGet("/", async (PokeQuizService service) => await service.GetMatchup());
 
 app.Run();
